73f4f7359a9e5921a068a9f6beee845d2024c130
kawaK <67514738+kawak1320@users.noreply.github.com>
2022-05-02 19:49:29 -0300

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,5 +1,5 @@
 const Game = require('../src/game').default
-const fs = require('fs')
+// const fs = require('fs')
 
 // describe('App', () => {
 //   it('Contains the compiled JavaScript', async () => {
